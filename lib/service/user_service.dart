06412cf5e1dd3f40acb3b3935b9624e2c80d8b9a
hyejinxx <haejinjung1110@gmail.com>
2023-06-22 22:33:26 +0900

    firedart 로 변경

--- lib/service/user_service.dart
+++ lib/service/user_service.dart
@@ -1,16 +1,17 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firedart/firedart.dart';
 
 import '../model/user.dart';
 
 class UserService {
-  FirebaseFirestore firestore = FirebaseFirestore.instance;
+  Firestore firestore = Firestore.instance;
 
   Future<List<User>> getAllUser() async {
     List<User> userList = [];
     try {
       await firestore.collection('user').get().then((value) {
-        value.docs.forEach((element) {
-          userList.add(User.fromJson(element.data()));
+        value.forEach((element) {
+          userList.add(User.fromJson(element.map));
         });
       });
       return userList;
@@ -21,10 +22,10 @@ class UserService {
 
   Future<User?> getUser(String userCall) async {
     try {
-      final data = await firestore.collection('user').doc(userCall).get();
-      if (data.exists && data.data() != null) {
-        print(User.fromJson(data.data()!));
-        return User.fromJson(data.data()!);
+      final data = await firestore.collection('user').document(userCall).get();
+      if (data.map.isNotEmpty && data!= null) {
+        print(User.fromJson(data.map!));
+        return User.fromJson(data.map!);
       } else {
         return null;
       }
@@ -35,15 +36,15 @@ class UserService {
 
   Future<void> editUser(Map<String, dynamic> data) async {
     try {
-      await firestore.collection('user').doc(data['call']).update(data);
+      await firestore.collection('user').document(data['call']).update(data);
     } catch (e) {
       throw Exception("editUser: $e");
     }
   }
   Future<void> deleteUser(String userCall) async {
     try {
-      await firestore.collection('user').doc(userCall).delete();
-      firestore.collection('user').doc('mileage').delete();
+      await firestore.collection('user').document(userCall).delete();
+      firestore.collection('user').document('mileage').delete();
 
     } catch (e) {
       throw Exception("deleteUser: $e");
