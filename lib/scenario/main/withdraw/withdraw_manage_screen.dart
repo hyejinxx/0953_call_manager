e68cfff8a2a5cee2ce0b3640ec792ab5a5b46e94
andyoung1226 <andyoung1226@gmail.com>
2023-06-28 23:51:50 +0900

    fix:  수정

--- lib/scenario/main/withdraw/withdraw_manage_screen.dart
+++ lib/scenario/main/withdraw/withdraw_manage_screen.dart
@@ -22,11 +22,16 @@ class _WithdrawManageScreenState extends ConsumerState<WithdrawManageScreen> {
   final lastDate = StateProvider((ref) => DateTime.now());
 
   WithdrawStatus isAll = WithdrawStatus.all;
-
+  final indexProvider = StateProvider<int>((ref) => 0);
+  final withdrawListProvider =
+      FutureProvider((ref) => MileageService().getWithdraw());
   final _controller = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
+    final index = ref.watch(indexProvider);
+    final withdrawList = ref.watch(withdrawListProvider);
+
     return SizedBox(
       width: MediaQuery.of(context).size.width,
       height: MediaQuery.of(context).size.height,
@@ -77,129 +82,157 @@ class _WithdrawManageScreenState extends ConsumerState<WithdrawManageScreen> {
           ],
         ),
         Expanded(
-            child: FutureBuilder(
-          future: MileageService().getWithdraw(),
-          builder: (context, snapshot) {
-            if (snapshot.hasData && snapshot.data != null) {
-              snapshot.data!.sort((a, b) => b.createdAt.compareTo(a.createdAt));
-              List<Withdraw> result = snapshot.data!.where((element) {
-                if (isAll == WithdrawStatus.all) {
-                  return true;
-                } else if (isAll == WithdrawStatus.waiting) {
-                  return element.status == '요청대기';
-                } else if (isAll == WithdrawStatus.completed) {
-                  return element.status == '출금완료';
-                } else if (isAll == WithdrawStatus.rejected) {
-                  return element.status!.contains('출금거절');
-                } else {
-                  return false;
-                }
-              }).toList();
-              return
-                SfDataGrid(
-                  onCellTap: (value) {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => WithdrawScreen(
-                              withdraw:result[value.rowColumnIndex.rowIndex-1],
-                            )));
-                  },
-                    defaultColumnWidth: MediaQuery.of(context).size.width/7, source: WithdrawSource(withdraw: result), columns: <GridColumn>[
-                  GridColumn(
-                      columnName: 'call',
-                      label: Container(
-                          padding: const EdgeInsets.all(8.0),
-                          alignment: Alignment.center,
-                          child: const Text(
-                            '전화번호',
-                            overflow: TextOverflow.ellipsis,
-                          ))),
-                  GridColumn(
-                      columnName: 'name',
-                      label: Container(
-                          padding: const EdgeInsets.all(8.0),
-                          alignment: Alignment.center,
-                          child: const Text(
-                            '예금주',
-                            overflow: TextOverflow.ellipsis,
-                          ))),
-                  GridColumn(
-                      columnName: 'amount',
-                      label: Container(
-                          padding: const EdgeInsets.all(8.0),
-                          alignment: Alignment.center,
-                          child: const Text(
-                            '요청금액',
-                            overflow: TextOverflow.ellipsis,
-                          ))),
-                  GridColumn(
-                      columnName: 'bank',
-                      label: Container(
-                          padding: const EdgeInsets.all(8.0),
-                          alignment: Alignment.center,
-                          child: const Text(
-                            '은행',
-                            overflow: TextOverflow.ellipsis,
-                          ))),
-                  GridColumn(
-                      columnName: 'account',
-                      label: Container(
-                          padding: const EdgeInsets.all(8.0),
-                          alignment: Alignment.center,
-                          child: const Text(
-                            '계좌번호',
-                            overflow: TextOverflow.ellipsis,
-                          ))),
-                  GridColumn(
-                      columnName: 'date',
-                      label: Container(
-                          padding: const EdgeInsets.all(8.0),
-                          alignment: Alignment.center,
-                          child: const Text(
-                            '요청일',
-                            overflow: TextOverflow.ellipsis,
-                          ))),
+            child: withdrawList.when(
+                data: (data) {
+                  if (data.isNotEmpty) {
+                    List<Withdraw> result = data!.where((element) {
+                      if (isAll == WithdrawStatus.all) {
+                        return true;
+                      } else if (isAll == WithdrawStatus.waiting) {
+                        return element.status == '요청대기';
+                      } else if (isAll == WithdrawStatus.completed) {
+                        return element.status == '출금완료';
+                      } else if (isAll == WithdrawStatus.rejected) {
+                        return element.status!.contains('출금거절');
+                      } else {
+                        return false;
+                      }
+                    }).toList();
 
-                  GridColumn(
-                      columnName: 'status',
-                      label: Container(
-                          padding: const EdgeInsets.all(8.0),
-                          alignment: Alignment.center,
-                          child: const Text(
-                            '출금상태',
-                            overflow: TextOverflow.ellipsis,
-                          ))),
-                ]);
+                    if (index == 0) {
+                      result.sort((a, b) => a.userCall.compareTo(b.userCall));
+                    } else if (index == 1) {
+                      result.sort((a, b) => a.name.compareTo(b.name));
+                    } else if (index == 2) {
+                      result.sort((a, b) => a.amount.compareTo(b.amount));
+                    } else if (index == 3) {
+                      result.sort((a, b) => a.bank.compareTo(b.bank));
+                    } else if (index == 4) {
+                      result
+                          .sort((a, b) => a.account.compareTo(b.account));
+                    } else if (index == 5) {
+                      result.sort((a, b) => a.createdAt.compareTo(b.createdAt));
+                    } else {
+                      result.sort((a, b) => a.status.compareTo(b.status));
+                    }
 
-            } else {
-              return const Center(
-                child: CircularProgressIndicator(),
-              );
-            }
-          },
-        ))
+                    return SfDataGrid(
+                        onCellDoubleTap: (detail) {
+                          if (detail.rowColumnIndex.rowIndex == 0) {
+                            ref.read(indexProvider.notifier).state =
+                                detail.rowColumnIndex.columnIndex;
+                          }
+                        },
+                        onCellTap: (value) {
+                          if (value.rowColumnIndex.rowIndex != 0) {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => WithdrawScreen(
+                                          withdraw: result[
+                                              value.rowColumnIndex.rowIndex -
+                                                  1],
+                                        )));
+                          }
+                        },
+                        defaultColumnWidth:
+                            MediaQuery.of(context).size.width / 7,
+                        source: WithdrawSource(withdraw: result.reversed.toList()),
+                        columns: <GridColumn>[
+                          GridColumn(
+                              columnName: 'call',
+                              label: Container(
+                                  padding: const EdgeInsets.all(8.0),
+                                  alignment: Alignment.center,
+                                  child: const Text(
+                                    '전화번호',
+                                    overflow: TextOverflow.ellipsis,
+                                  ))),
+                          GridColumn(
+                              columnName: 'name',
+                              label: Container(
+                                  padding: const EdgeInsets.all(8.0),
+                                  alignment: Alignment.center,
+                                  child: const Text(
+                                    '예금주',
+                                    overflow: TextOverflow.ellipsis,
+                                  ))),
+                          GridColumn(
+                              columnName: 'amount',
+                              label: Container(
+                                  padding: const EdgeInsets.all(8.0),
+                                  alignment: Alignment.center,
+                                  child: const Text(
+                                    '요청금액',
+                                    overflow: TextOverflow.ellipsis,
+                                  ))),
+                          GridColumn(
+                              columnName: 'bank',
+                              label: Container(
+                                  padding: const EdgeInsets.all(8.0),
+                                  alignment: Alignment.center,
+                                  child: const Text(
+                                    '은행',
+                                    overflow: TextOverflow.ellipsis,
+                                  ))),
+                          GridColumn(
+                              columnName: 'account',
+                              label: Container(
+                                  padding: const EdgeInsets.all(8.0),
+                                  alignment: Alignment.center,
+                                  child: const Text(
+                                    '계좌번호',
+                                    overflow: TextOverflow.ellipsis,
+                                  ))),
+                          GridColumn(
+                              columnName: 'date',
+                              label: Container(
+                                  padding: const EdgeInsets.all(8.0),
+                                  alignment: Alignment.center,
+                                  child: const Text(
+                                    '요청일',
+                                    overflow: TextOverflow.ellipsis,
+                                  ))),
+                          GridColumn(
+                              columnName: 'status',
+                              label: Container(
+                                  padding: const EdgeInsets.all(8.0),
+                                  alignment: Alignment.center,
+                                  child: const Text(
+                                    '출금상태',
+                                    overflow: TextOverflow.ellipsis,
+                                  ))),
+                        ]);
+                  } else {
+                    return Center(
+                      child: Text('출금 내역이 없습니다.'),
+                    );
+                  }
+                },
+                loading: () => const Center(child: CircularProgressIndicator()),
+                error: (error, stack) {
+                  return Center(
+                    child: Text(error.toString()),
+                  );
+                }))
       ]),
     );
   }
 }
-
 
 class WithdrawSource extends DataGridSource {
   /// Creates the employee data source class with required details.
   WithdrawSource({required List<Withdraw> withdraw}) {
     _withdraw = withdraw
         .map<DataGridRow>((e) => DataGridRow(cells: [
-      DataGridCell<String>(columnName: 'call', value: e.userCall),
-      DataGridCell<String>(columnName: 'name', value: e.name),
-      DataGridCell<int>(columnName: 'amount', value: e.amount),
-      DataGridCell<String>(columnName: 'bank', value: e.bank),
-      DataGridCell<String>(
-          columnName: 'account', value: e.account),
-      DataGridCell<String>(
-            columnName: 'date', value: e.createdAt),
-      DataGridCell<String>(columnName: 'status', value: e.status!),
-    ]))
+              DataGridCell<String>(columnName: 'call', value: e.userCall),
+              DataGridCell<String>(columnName: 'name', value: e.name),
+              DataGridCell<int>(columnName: 'amount', value: e.amount),
+              DataGridCell<String>(columnName: 'bank', value: e.bank),
+              DataGridCell<String>(columnName: 'account', value: e.account),
+              DataGridCell<String>(columnName: 'date', value: e.createdAt),
+              DataGridCell<String>(columnName: 'status', value: e.status!),
+            ]))
         .toList();
   }
 
@@ -212,11 +245,11 @@ class WithdrawSource extends DataGridSource {
   DataGridRowAdapter buildRow(DataGridRow row) {
     return DataGridRowAdapter(
         cells: row.getCells().map<Widget>((e) {
-          return Container(
-            alignment: Alignment.center,
-            padding: const EdgeInsets.all(8.0),
-            child: Text(e.value.toString()),
-          );
-        }).toList());
+      return Container(
+        alignment: Alignment.center,
+        padding: const EdgeInsets.all(8.0),
+        child: Text(e.value.toString()),
+      );
+    }).toList());
   }
 }
